395e1ddc1dcf30ba11d61d262bb0b946fd084c19
ehsanfld <ehsan.fld@gmail.com>
2023-07-22 00:12:45 +0330

    added all the src files some of which were missing in the premake file.

--- premake5.lua
+++ premake5.lua
@@ -6,17 +6,12 @@ project "glfw"
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
-	{
-		"include/glfw/glfw3.h",
-		"include/glfw/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+	{	
+		"src/**.h",
+		"src/**.c",
+		"src/**.cpp"
 	}
+
 	filter "system:linux"
 		pic "On"
 
